c40645fe1250fa7f781d45164cbb7703959750f8
Mark J. Olah <markjolah@users.noreply.github.com>
2019-08-30 14:18:14 -0600

    Update python dependencies for workflow and reorganize pip installs

--- buildscripts/libs/build_pyjedi.sh
+++ buildscripts/libs/build_pyjedi.sh
@@ -13,19 +13,38 @@ name="pyjedi"
 # Python Package installs
 #####################################################################
 
-$SUDO python -m pip install -U pip setuptools
+## Python 2
+$SUDO python -m pip install -U pip
+$SUDO python -m pip install -U setuptools
+$SUDO python -m pip install -U wheel
 $SUDO python -m pip install -U numpy
-$SUDO python -m pip install -U wheel netCDF4 matplotlib
+$SUDO python -m pip install -U netCDF4
+$SUDO python -m pip install -U matplotlib
 
-$SUDO python3 -m pip install -U pip setuptools
-$SUDO python3 -m pip install -U numpy
-$SUDO python3 -m pip install -U wheel netCDF4 matplotlib
-$SUDO python3 -m pip install -U pandas
+## Python 3 - General packages
+$SUDO python3 -m pip install -U pip
+$SUDO python3 -m pip install -U setuptools
+$SUDO python3 -m pip install -U wheel
 $SUDO python3 -m pip install -U pycodestyle
 $SUDO python3 -m pip install -U autopep8
 $SUDO python3 -m pip install -U cffi
 $SUDO python3 -m pip install -U pycparser
 $SUDO python3 -m pip install -U pytest
+
+## Python 3 - Numerical packages
+$SUDO python3 -m pip install -U numpy
+$SUDO python3 -m pip install -U netCDF4
+$SUDO python3 -m pip install -U matplotlib
+$SUDO python3 -m pip install -U pandas
+$SUDO python3 -m pip install -U xarray
+
+## Python 3 - Workflow packages
+$SUDO python3 -m pip install -U click
+$SUDO python3 -m pip install -U ruamel
+$SUDO python3 -m pip install -U jinja
+$SUDO python3 -m pip install -U boto3
+$SUDO python3 -m pip install -U progressbar2
+
 
 #####################################################################
 # ncepbufr for python
